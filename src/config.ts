74fac642da91ec5ee056515ad8f32e7d9198c2ed
Manuel Montenegro <manuel@nucypher.com>
2023-11-18 19:23:53 +0300

    Change default Ritual ID and domain

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ import { domains } from '@nucypher/taco';
 
 export const DEFAULT_RITUAL_ID =
   (process.env.DEFAULT_RITUAL_ID && parseInt(process.env.DEFAULT_RITUAL_ID)) ||
-  24;
+  5;
 
-export const DEFAULT_DOMAIN = process.env.DEFAULT_DOMAIN || domains.DEV;
+export const DEFAULT_DOMAIN = process.env.DEFAULT_DOMAIN || domains.TESTNET;
